a534e152797a4f08fa623cf517f603e1f5cac1fa
jameshale14 <james.hale.1890@googlemail.com>
2018-03-09 16:44:37 +0000

    Updated to version 1.2 - Removed tooltips, they were annoying because they got stuck on a touchscreen - Re-enabled pull-to-refresh so the app can update. Fixed the offline refresh showing a broken offline screen. - Implemented deletion of old caches

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,4 +1,10 @@
 # Changelog 
+
+## Version 1.2
+- Removed tooltips, they were annoying because they got stuck on a touchscreen
+- Re-enabled pull-to-refresh so the app can update. Fixed the offline refresh showing a broken offline screen.
+- Implemented deletion of old caches
+
 ## Version 1.1
 - Added info panel with attributions to QRCode library and Material Design Lite
 - Disabled pull-to-refresh
--- public/sw.js
+++ public/sw.js
@@ -1,34 +1,48 @@
+var staticCacheName = 'speedlane-v1.2';
+
+var filesToCache = [
+  '/',
+  '/index.html',
+  '/js/index.js',
+  '/images/logo.png',
+  '/lib/qrcode.min.js',
+  '/lib/material_icons.css',
+  '/lib/material_icons.woff2',
+  '/lib/material.min.css',
+  '/lib/material.min.js',
+  '/webManifest.json'
+];
+
 self.addEventListener('install', function (e) {
-    e.waitUntil(
-      caches.open('speedlane-v1.1').then(function (cache) {
-        return cache.addAll([
-          '/',
-          '/index.html',
-          '/js/index.js',
-          '/images/logo.png',
-          '/lib/qrcode.min.js',
-          '/lib/material_icons.css',
-          '/lib/material_icons.woff2',
-          '/lib/material.min.css',
-          '/lib/material.min.js',
-          '/webManifest.json'
-        ]);
-      })
-    );
-  });
-  
-  self.addEventListener('fetch', function (event) {
-  
-    console.log(event.request.url);
-  
-    event.respondWith(
-  
-      caches.match(event.request).then(function (response) {
-  
-        return response || fetch(event.request);
-  
-      })
-  
-    );
-  
-  });
+  e.waitUntil(
+    caches.open(staticCacheName).then(function (cache) {
+      return cache.addAll(filesToCache);
+    })
+  );
+});
+
+self.addEventListener('fetch', function (event) {
+  event.respondWith(
+    caches.match(event.request).then(function (response) {
+      return response || fetch(event.request);
+    })
+  );
+});
+
+self.addEventListener('activate', function (event) {
+  console.log('Activating new service worker...');
+
+  var cacheWhitelist = [staticCacheName];
+
+  event.waitUntil(
+    caches.keys().then(function (cacheNames) {
+      return Promise.all(
+        cacheNames.map(function (cacheName) {
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
